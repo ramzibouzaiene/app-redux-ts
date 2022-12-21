dff663402f6da8af66ecdc8407d6ba7d660857ca
ramzibouzaiene <ramzibouzaiene00@gmail.com>
2022-12-21 23:52:52 +0100

    adding the project with folders structure of redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators, State } from './state';
+
 
 function App() {
+
+  const dispatch = useDispatch();
+  const {depositMoney, withdrawMoney, bankrupt} = bindActionCreators(actionCreators, dispatch)
+  const amount = useSelector((state: State) => state.bank)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{amount}</h1>
+      <button onClick={() => depositMoney(1000)}>Deposit</button>
+      <button onClick={() => withdrawMoney(500)}>Withdraw</button>
+      <button onClick={() => bankrupt()}>Bankrupt</button>
     </div>
   );
 }
